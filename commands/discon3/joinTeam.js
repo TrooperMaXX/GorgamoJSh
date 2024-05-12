9cebf2d8718ecc479a03056a262b233410e3c6ef
TrooperMaXX <trooper@troopermaxx.de>
2024-05-13 01:41:17 +0200

    quotefix + spacefix

--- commands/discon3/joinTeam.js
+++ commands/discon3/joinTeam.js
@@ -5,9 +5,8 @@ module.exports = {
 		.setName('jointeam')
 		.setDescription('Tritt einem Team für die Discon 3 bei!'),
 	async execute(interaction) {
-		const replyContent = `Man kan keinen Teams mehr beitreten! Lang leben die **Siegreichen Phönixe**`;
-					await interaction.reply({ content: replyContent, ephemeral: true });
-			
+		const replyContent = 'Man kan keinen Teams mehr beitreten! Lang leben die **Siegreichen Phönixe**';
+		await interaction.reply({ content: replyContent, ephemeral: true });
 
 	},
 };
