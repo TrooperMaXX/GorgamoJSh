adaa69e4b7bdb154895f43dafe92f779d48898be
TrooperMaXX <trooper@troopermaxx.de>
2024-09-23 00:14:20 +0200

    typo fix  + scammer notification in # prison

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -5,7 +5,7 @@ module.exports = {
 	async execute(oldMember, newMember) {
 
 		if (!oldMember.roles.cache.some(role => role.name === 'Server Neuling') && newMember.roles.cache.some(role => role.name === 'Server Neuling')) {
-			const buddyChanel = newMember.client.channels.cache.get('1225754090299064340');
+			const buddyChannel = newMember.client.channels.cache.get('1225754090299064340');
 			const helpTxts = [ 'braucht Hilfe!', 'hat sich im Wald verirrt :o', 'hat eine Nat 1 auf Orientierung gewürfelt!', 'findet nicht die Richtigen Würfel', 'hat nicht einmal den Quantumoger gefunden', 'Möchte aus dem Bälleparadies abgeholt werden', 'Ist im Turm gefangen'];
 			const choosenHelpTxt = helpTxts[Math.floor(Math.random() * helpTxts.length)];
 			const exampleEmbed = new EmbedBuilder()
@@ -14,7 +14,20 @@ module.exports = {
 				.setDescription(`<@${newMember.id}> ${choosenHelpTxt}`)
 				.setTimestamp();
 
-			buddyChanel.send({ content: 'Hey <@&1230570229369667584>', embeds: [exampleEmbed] });
+			buddyChannel.send({ content: 'Hey <@&1230570229369667584>', embeds: [exampleEmbed] });
+		}
+
+		if (!oldMember.roles.cache.some(role => role.name === 'Scammer') && newMember.roles.cache.some(role => role.name === 'Scammer')) {
+			const buddyChannel = newMember.client.channels.cache.get('1286838131349262387');
+			const helpTxts = [ 'will leute scammen'];
+			const choosenHelpTxt = helpTxts[Math.floor(Math.random() * helpTxts.length)];
+			const exampleEmbed = new EmbedBuilder()
+				.setColor(0xff8800)
+				.setAuthor({ name: newMember.displayName, iconURL: newMember.displayAvatarURL() })
+				.setDescription(`<@${newMember.id}> ${choosenHelpTxt}`)
+				.setTimestamp();
+
+			buddyChannel.send({ content: '', embeds: [exampleEmbed] });
 		}
 
 	},
